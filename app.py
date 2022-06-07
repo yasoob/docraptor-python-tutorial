c2e70a88ffcd5cc9c8aa01b48119aa3ab8fd8740
Yasoob Khalid <hi@yasoob.me>
2022-06-08 00:56:04 +0500

    modified the code slightly

--- app.py
+++ app.py
@@ -3,19 +3,26 @@ import os
 
 doc_api = docraptor.DocApi()
 doc_api.api_client.configuration.username = os.environ.get("DOCRAPTOR_KEY")
-doc_api.api_client.configuration.debug = True
 
 with open("invoice.html", "r") as f:
     invoice_html = f.read()
 
-response = doc_api.create_doc(
-    {
-        "test": True,
-        "document_content": invoice_html,
-        "name": "invoice.pdf",
-        "document_type": "pdf",
-    }
-)
+try:
+    response = doc_api.create_doc(
+        {
+            "test": True,
+            "document_content": invoice_html,
+            "name": "invoice.pdf",
+            "document_type": "pdf",
+        }
+    )
 
-with open("invoice.pdf", "wb") as f:
-    f.write(response)
+    with open("invoice.pdf", "w+b") as f:
+        binary_formatted_response = bytearray(response)
+        f.write(binary_formatted_response)
+        f.close()
+
+except docraptor.rest.ApiException as error:
+    print(error.status)
+    print(error.reason)
+    print(error.body)
